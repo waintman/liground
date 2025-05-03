6b631b1cab849633a2f801f20e165e346546ae77
YEON SEOK CHOI <yeonseok.wm.choi@gmail.com>
2025-05-04 07:58:40 +0900

    fix clipboard

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,8 +3,10 @@ import App from './App'
 import router from './router'
 import { store } from './store'
 import { ContextMenu } from '@imengyu/vue3-context-menu'
-createApp(App)
-  .component('ContextMenu', ContextMenu)
+const { clipboard } = require('electron')
+const app = createApp(App)
+app.config.globalProperties.$electron = { clipboard }
+app.component('ContextMenu', ContextMenu)
   .use(router)
   .use(store)
   .mount('#app')
